96404b5956c7749f9bc27cead73b8716b1939694
Dan Chung <danetix@gmail.com>
2017-07-19 13:33:02 -0400

    dialog sends data to firebase

--- src/components/dialog_add.js
+++ src/components/dialog_add.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import * as firebase from 'firebase';
 
 import Dialog from 'material-ui/Dialog';
 import RaisedButton from 'material-ui/RaisedButton';
@@ -9,6 +10,7 @@ import MenuItem from 'material-ui/MenuItem';
 import Divider from 'material-ui/Divider';
 
 const numChildItems = [];
+// populate num child nodes select dropdown
 const maxChildren = 15;
 for (let i = 0; i <= maxChildren; i++)
   numChildItems.push(<MenuItem value={i} key={i} primaryText={`${i}`} />)
@@ -20,7 +22,50 @@ export default class AddDialog extends Component {
 
   handleOpen = () => { this.setState({ open: true }); };
   handleClose = () => { this.setState({ open: false }); };
-  handleChange = (event, index, value) => { this.setState({value}); };
+  handleChange = (event, index, value) => {
+    const target = event.target;
+    const name = target.name;
+    // use name attr to determine if SelectField
+    name ? (this.setState({ [name]: target.value })) : this.setState({ parentQuantity: value });
+  };
+
+  handleSubmit = () => {
+    var data = [{
+      parentLabel: this.state.parentLabel,
+      parentLower: this.state.parentLower,
+      parentUpper: this.state.parentUpper,
+      parentQuantity: this.state.parentQuantity,
+      parentChildren: this.generateChildNodes(
+        this.state.parentLower,
+        this.state.parentUpper,
+        this.state.parentQuantity),
+    }];
+    // if this.props.parentNodes is empty, then do not concat
+    this.props.parentNodes ? data = this.props.parentNodes.concat(data) : null;
+    this.writeNodeData(data);
+    this.setState({ open: false });
+  }
+
+  generateChildNodes(lower, upper, quantity) {
+    var childValueArray = [];
+    var i;
+    for(i = 0; i < quantity; i++) {
+      var childValue = this.generateRandomNumber(lower, upper);
+      childValueArray.push(childValue);
+    }
+
+    return childValueArray;
+  }
+
+  generateRandomNumber(min, max) { // both inclusive
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max-min+1)) + min;
+  }
+
+  writeNodeData(data) {
+    firebase.database().ref('nodes').set(data);
+  }
 
   render() {
     const actions = [
@@ -30,10 +75,10 @@ export default class AddDialog extends Component {
         onTouchTap={this.handleClose}
       />,
       <FlatButton
-        label="Create"
+        label="Add"
         primary={true}
         keyboardFocused={true}
-        onTouchTap={this.handleClose}
+        onTouchTap={this.handleSubmit}
       />
     ];
 
@@ -48,12 +93,14 @@ export default class AddDialog extends Component {
           onRequestClose={this.handleClose}
         >
           <TextField
+            name="parentLabel"
+            onChange={this.handleChange}
             hintText="Parent Label"
           />
           <br />
           <SelectField
             floatingLabelText="How many child nodes?"
-            value={this.state.value}
+            value={this.state.parentQuantity}
             onChange={this.handleChange}
             autoWidth={true}
           >
@@ -61,11 +108,15 @@ export default class AddDialog extends Component {
           </SelectField>
           <br />
           <TextField
+            name="parentLower"
+            onChange={this.handleChange}
             hintText="0"
             floatingLabelText="Child node min value"
             floatingLabelFixed={true}
           />&nbsp;
           <TextField
+            name="parentUpper"
+            onChange={this.handleChange}
             hintText="1000"
             floatingLabelText="Child node max value"
             floatingLabelFixed={true}
--- src/components/node_root.js
+++ src/components/node_root.js
@@ -7,47 +7,16 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDlGymK7eK3hlYLgYyIkN_psbbjGSz74FY",
-  authDomain: "passport-challenge-92a65.firebaseapp.com",
-  databaseURL: "https://passport-challenge-92a65.firebaseio.com",
-  projectId: "passport-challenge-92a65",
-  storageBucket: "passport-challenge-92a65.appspot.com",
-  messagingSenderId: "1077073664940",
-};
-firebase.initializeApp(firebaseConfig);
-
 var parentNodes = [];
 
 export default class NodeRoot extends Component {
   constructor(props) {
     super();
 
-    this.state = {
-      parentNodes: '',
-    };
-
-    this.addParent = this.addParent.bind(this);
-  }
-
-  componentDidMount() {
-    // get data from firebase and setState
-    const dbRefObject = firebase.database().ref().child('nodes');
-    dbRefObject.on('value', snap => {
-      if(snap.val()) {
-        this.setState({
-          parentNodes: snap.val(),
-        });
-      } else {
-        this.setState({
-          parentNodes: '',
-        });
-      }
-    });
   }
 
   renderNodes() {
-    let nodes = this.state.parentNodes;
+    let nodes = this.props.parentNodes;
     // iterate through nodes data object
     return Object.keys(nodes).map(function(keyName, keyIndex) {
       let obj = nodes[keyName];
@@ -57,11 +26,11 @@ export default class NodeRoot extends Component {
           <ol>
             {
               // mapping through child nodes for each parent here
-              obj.parentChildren.map(function(props, index) {
+              obj.parentChildren ? obj.parentChildren.map(function(props, index) {
                 return (
                   <li className="node-child" key={index}>{props}</li>
                 );
-              })
+              }) : null
             }
           </ol>
         </li>
@@ -69,57 +38,11 @@ export default class NodeRoot extends Component {
     });
   }
 
-  addParent() {
-    // temporary to generate dummy data
-    var label = this.generateRandomNumber(0,1000);
-    var lower = this.generateRandomNumber(0,9);
-    var upper = this.generateRandomNumber(10,100);
-    var quantity = this.generateRandomNumber(1,15);
-    var children = this.generateChildNodes(lower, upper, quantity);
-
-    if( this.state.parentNodes ) {
-      parentNodes = this.state.parentNodes.concat({
-        parentLabel: label,
-        parentLower: lower,
-        parentUpper: upper,
-        parentQuantity: quantity,
-        parentChildren: children
-      });
-    }
-
-    this.setState({
-      parentNodes: parentNodes,
-    });
-
-    this.writeNodeData(parentNodes);
-  }
-
-  generateChildNodes(lower, upper, quantity) {
-    var childValueArray = [];
-    var i;
-    for(i = 0; i < quantity; i++) {
-      var childValue = this.generateRandomNumber(lower, upper);
-      childValueArray.push(childValue);
-    }
-
-    return childValueArray;
-  }
-
-  generateRandomNumber(min, max) { // both inclusive
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max-min+1)) + min;
-  }
-
-  writeNodeData(data) {
-    firebase.database().ref('nodes').set(data);
-  }
-
   render() {
     return(
       <ul className="node-root">
         This is the root view.
-        <button onTouchTap={this.addParent}> + </button>
+        {/* <button onTouchTap={this.addParent}> + </button> */}
         {this.renderNodes()}
       </ul>
     );
